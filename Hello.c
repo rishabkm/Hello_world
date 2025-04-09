61df45857bf54ac71c81b8f1c1593988b6145310
rishabkm <119279619+rishabkm@users.noreply.github.com>
2025-04-09 22:44:18 +0530

    Update Hello.c

--- Hello.c
+++ Hello.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 int main(){
+    int a=1,b=2;
     printf("Hello! World");
     printf("Rishabh will dominate this world.");
     printf("A is a");
+    printf("sum of A and B is: %d",a+b);
     return 0;
 }
